40b7025c7f65c1a5a0c0323121a482e08fd0180c
foreversmart <njutree@gmail.com>
2015-12-05 16:04:13 +0800

    add positioner interface

--- filestream/position.go
+++ filestream/position.go
@@ -1,5 +1,42 @@
 package filestream
 
 import (
-    ""
+	"io"
+	"os"
 )
+
+type Positioner interface {
+	SeekInFile() (io.Reader, error)
+	Move(int)
+}
+
+type Position struct {
+	SeekPos int64  `json:"seek_position"`
+	Path    string `json:"path"`
+}
+
+func (pos *Position) SeekInFile() (reader io.Reader, err error) {
+	fd, err := os.Open(pos.Path)
+	if err != nil {
+		return
+	}
+
+	reader = fd
+
+	// Try to get to our seek position, if our seek is 0, then start at the
+	// beginning.
+	if pos.SeekPos == 0 {
+		return
+	}
+
+	_, err = fd.Seek(pos.SeekPos, 0)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (pos *Position) Move(n int64) {
+	pos.SeekPos += n
+}
